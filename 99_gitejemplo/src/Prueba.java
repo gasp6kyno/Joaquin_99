45a149b26bedc2f50cc448b048e34ec9b5ccc9be
Ana <Curso maana@PC22a5>
2018-01-29 12:53:28 +0100

    Cambio de Ana

--- 99_gitejemplo/src/Prueba.java
+++ 99_gitejemplo/src/Prueba.java
@@ -4,7 +4,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Recibido");
+		System.out.println("Primera prueba");
+		System.out.println("Primer cambio");
 	}
 
 }
